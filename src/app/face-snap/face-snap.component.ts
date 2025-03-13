a882de7a14da2c57ab7209e5d0f9892a86ad60ab
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-13 15:43:52 +0100

    create face-snap TS

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,50 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FaceSnap} from "../model/face-snap";
+import {DatePipe, NgClass, NgStyle, TitleCasePipe, UpperCasePipe} from "@angular/common";
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [
+    NgStyle,
+    NgClass,
+    TitleCasePipe,
+    UpperCasePipe,
+    DatePipe
+  ],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
+
+  snapButtonText!: string;
+  snapped!: boolean;
+
+
+  ngOnInit() {
+
+  }
+
+  onAddSnap(): void {
+    if (this.snapped) {
+      this.unSnap()
+    } else {
+      this.snap()
+    }
+  }
+
+  unSnap() {
+    this.faceSnap.removeSnap();
+    this.snapButtonText = 'Oh Snap!';
+    this.snapped = false;
+  }
+
+  snap() {
+    this.faceSnap.addSnap();
+    this.snapButtonText = 'Unsnap!';
+    this.snapped = true;
+  }
+
+  protected readonly location = location;
+}
